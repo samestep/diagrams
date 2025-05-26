94179ecb5365962fb8bf153302f578836df8f258
Sam Estep <sam@samestep.com>
2025-05-26 15:13:42 -0700

    Add a Nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell =
+          with pkgs;
+          mkShellNoCC {
+            buildInputs = [
+              nixfmt-rfc-style
+              python3
+              uv
+            ];
+          };
+      }
+    );
+}
